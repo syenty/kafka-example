18682ce0d1d40d6877ba8eb3763fec7a9fa8883b
syenty <syenty@imform.co.kr>
2025-09-13 01:38:32 +0900

    feat: add each schema feat: update common build.gradle

---
+++ apps/inventory/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE inventory
+(
+    id    VARCHAR(255) PRIMARY KEY,
+    stock INT NOT NULL
+);
---
+++ apps/payment/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE payments
+(
+    id       BIGSERIAL PRIMARY KEY,
+    order_id BIGINT      NOT NULL,
+    status   VARCHAR(50) NOT NULL -- e.g., 'PENDING', 'SUCCESS', 'FAILED'
+);
